c2e9f1713a7156c8cc0fcccb314d244e70296847
DragonFlyEye <DragonFlyEye@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-27 14:05:05 +0000

    Adding 3.0 directory to svn
    git-svn-id: https://plugins.svn.wordpress.org/title-to-tags/tags/3.0@316079 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- hn_title_to_tags.php
+++ hn_title_to_tags.php
@@ -3,7 +3,7 @@
 Plugin Name: Title to Tags
 Plugin URI: http://holisticnetworking.net/plugins/2008/01/25/the-titles-to-tags-plugin/
 Description: Creates tags for posts based on the post title on update or publish.
-Version: 2.0
+Version: 3.0
 Author: Thomas J. Belknap
 Author URI: http://holisticnetworking.net
 */
@@ -25,128 +25,127 @@ Author URI: http://holisticnetworking.net
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-/*
-Change Log:
-    * 1.0 ~ Initial public release
-    * 1.1 ~ Corrected some meta data
-    * 1.2 ~ Name collision with another plugin, FeedWordPress, corrected
-    * 1.3 ~ Whoops! Didn't put the title in the meta data, how silly!
-    * 1.4 ~ SVN commit to include tag
-	* 2.0 ~ Revamped version based on experiments with these function in another plugin.
-			~ using WP-style function notes
-			~ designed to work with both WP and WPMU
-			~ lowerNoPunc function now removes posessive 's from words
-			~ addresses issue where tags are created even if they're not added to the post
-*/
+// Get out there and rock and roll the bones:
+$tt	= new titleToTags();
 
-/**
- * Gather keywords from title and create post tags for them
- *
- * @uses wp_set_post_tags()
- *
- * @param int $post_id Post ID.
- */
-function titleToTags($post_id) {
-	// Fix for auto-save and revision IDs:
-	if(wp_is_post_revision($post_id) || wp_is_post_autosave($post_id)){
-		return $post_id;
-	} else {
-		$post = get_post($post_id);
-		if(wp_get_post_tags($post_id)) {
-			return false;
-		}
-		$title = $post->post_title;
-		$stopwords = get_option('hn_title_to_tags');
-		// do we have stopwords in the db?  If not, use the default list:
-		if ( !$stopwords ) {
-			$file = dirname(__FILE__).'/hn_t2t/stopwords.txt';
-			$defaults = file_get_contents($file);
-			$stopwords = $defaults;
-			update_option('hn_title_to_tags', $stopwords);
-		}
-		// get our array of stop words and words in the title.  Compare arrays and select tags:
-		$verboten = explode(',', $stopwords);
-		$title_werdz = explode(' ', $title);
-		for($x = 0; $x < count($verboten); $x++) {
-			$verboten[$x] = lowerNoPunc($verboten[$x]);
-		}
-		foreach ($title_werdz as $werd) {
-			$werd = lowerNoPunc($werd); //trim(preg_replace('#[^\p{L}\p{N}]+#u', '', $werd));
-			if(!in_array($werd, $verboten)) {
-				$tags[] = $werd;
-			}
-		}
-		// Finally, add the tags to the post
-		wp_add_post_tags($post_id, $tags);
-	}
-}
-	
-	
-	
-/**
- * Convert words to all lower case, eliminate punctuation, remove posessives
- *
- * @param string $werd the word to be converted
- * @return string converted word
- */
-function lowerNoPunc($werd) {
-	if(stristr($werd, "'s")) {
-		$sploded = explode("'", $werd);
-		$werd = $sploded[0];
-	}
-	$werd = strtolower(trim(preg_replace('#[^\p{L}\p{N}]+#u', '', $werd)));
-	return $werd;
-}
-	
-	
-	
-function titleToTags_control() {
+class titleToTags {
 
-	// Get our options and see if we're handling a form submission.
-	$options = get_option('hn_title_to_tags');
-	if ( !is_array($options) ){
-		$stopwords = dirname(__FILE__).'/hn_t2t/stopwords.txt';
-		$defaults = file_get_contents($stopwords);
-		$options = array('hnt2t_exceptions'=>$defaults);
+	/*
+	// convert:				Does the business of converting titles to tags.
+	// @var int $post_id:	The ID of the post whose title is in need of conversion.
+	*/
+	public function convert($post_id) {
+		// Fix for auto-save and revision IDs:
+		if(wp_is_post_revision($post_id) || wp_is_post_autosave($post_id)) :
+			return $post_id;
+		else :
+			// Get the post:
+			$post = get_post($post_id);
+			if($title = $post->post_title) :
+				// This is a total cluge, but it works. There must be a better post status to use, but haven't found it yet.
+				if($title != 'Auto Draft') :
+					// We only commit new post tags if there are no previous post tags:
+					if(!wp_get_post_tags($post_id)) :
+						// Setup our tag data:
+						$title_to_tags	= array();
+						$stopwords		= $this->getStopWords();
+						$title_werdz	= explode(' ', $title);
+						foreach ($title_werdz as $werd) :
+							$werd = $this->lowerNoPunc($werd);
+							if(!in_array($werd, $stopwords)) :
+								$title_to_tags[] = $werd;
+							endif;
+						endforeach;
+						// Finally, add the tags to the post
+						wp_add_post_tags($post_id, $title_to_tags);
+					endif;
+				endif;
+			endif;
+		endif;
 	}
-	if ( $_POST['hnt2t-submit'] ) {
-		if ($_POST['hnt2t_reset'] == 1) {
+	
+	
+	public function control() {
+		// Get our options and see if we're handling a form submission.
+		$options = get_option('hn_title_to_tags');
+		if ( !is_array($options) ) :
 			$stopwords = dirname(__FILE__).'/hn_t2t/stopwords.txt';
-			$options['hnt2t_exceptions'] = file_get_contents($stopwords);	
-		}
-		else { $options['hnt2t_exceptions'] = strip_tags(stripslashes($_POST['hnt2t_exceptions'])); }
-		update_option('hn_title_to_tags', $options);
-		?><div id='message' class='updated fade'><p><strong>Title to Tags exception list updated!</strong></p></div><?php
+			$defaults = file_get_contents($stopwords);
+			$options = array('hnt2t_exceptions'=>$defaults);
+		endif;
+		if ( isset($_POST['hnt2t-submit']) ) :
+			if (isset($_POST['hnt2t_reset'])) :
+				$stopwords = dirname(__FILE__).'/hn_t2t/stopwords.txt';
+				$options['hnt2t_exceptions'] = file_get_contents($stopwords);	
+			else : 
+				$options['hnt2t_exceptions'] = strip_tags(stripslashes($_POST['hnt2t_exceptions']));
+			endif;
+			update_option('hn_title_to_tags', $options);
+			?><div id='message' class='updated fade'><p><strong>Title to Tags exception list updated!</strong></p></div><?php
+		endif;
+
+		// Be sure you format your options to be valid HTML attributes.
+		$exceptions = htmlspecialchars($options['hnt2t_exceptions'], ENT_QUOTES);
+	
+		// Begin form output
+	?>
+	<div class="wrap">
+	<h2>Title to Tags</h2>
+		<form id="hn_title_to_tags" name="hn_title_to_tags" method="post" action="options-general.php?page=hn_title_to_tags.php">
+		<h3>Excluded Words</h3>
+		<p>These words will be ignored by Title to Tags</p>
+		<textarea rows="6" cols="100" name="hnt2t_exceptions" id="hnt2t_exceptions"><?php echo($exceptions); ?></textarea>
+		<h3>Reset Excluded Words</h3>
+		<p><strong>Warning!!</strong>  Setting this option will restore your ignore list to it's original state and delete any entries you may have made.  Please only use this option when you are <strong>sure</strong> you want to reset!</p>
+		<input type="checkbox" name="hnt2t_reset" id="hnt2t_reset" value="1"><label for="hnt2t_reset">Reset all ignore words to defaults</label><br /><br />
+		<input type="submit" name="submit" id="submit" value="submit" />
+		<input type="hidden" id="hnt2t-submit" name="hnt2t-submit" value="1" />
+		</form>
+	</div>
+	<?php
 	}
-
-	// Be sure you format your options to be valid HTML attributes.
-	$exceptions = htmlspecialchars($options['hnt2t_exceptions'], ENT_QUOTES);
 	
-	// Begin form output
-?>
-<div class="wrap">
-<h2>Title to Tags</h2>
-	<form id="hn_title_to_tags" name="hn_title_to_tags" method="post" action="options-general.php?page=hn_title_to_tags.php">
-	<h3>Excluded Words</h3>
-	<p>These words will be ignored by Title to Tags</p>
-	<textarea rows="6" cols="100" name="hnt2t_exceptions" id="hnt2t_exceptions"><?php echo($exceptions); ?></textarea>
-	<h3>Reset Excluded Words</h3>
-	<p><strong>Warning!!</strong>  Setting this option will restore your ignore list to it's original state and delete any entries you may have made.  Please only use this option when you are <strong>sure</strong> you want to reset!</p>
-	<input type="checkbox" name="hnt2t_reset" id="hnt2t_reset" value="1"><label for="hnt2t_reset">Reset all ignore words to defaults</label><br /><br />
-	<input type="submit" name="submit" id="submit" value="submit" />
-	<input type="hidden" id="hnt2t-submit" name="hnt2t-submit" value="1" />
-	</form>
-</div>
-<?php
-
-}
-
-function hn_t2t_add_menu() {
-	add_options_page('Title to Tags', 'Title 2 Tags', 8, basename(__FILE__),'titleToTags_control');
-}
+	public function addMenu() {
+		add_options_page('Title to Tags', 'Title 2 Tags', 'edit_posts', basename(__FILE__), array( &$this, 'control'));
+	}
 	
-add_action('save_post', 'titleToTags', 2);
-add_action('admin_menu', 'hn_t2t_add_menu');
-
-
+	function __construct() {
+		add_action('save_post', array(&$this, 'convert'));
+		add_action('admin_menu', array(&$this, 'addMenu'));
+	}
+	
+	/*
+	// lowerNoPunc:				Converts all words into lower-case words, sans punctuation or possessives.
+	*/
+	private function lowerNoPunc($werd) {
+		if(stristr($werd, "'s")) :
+			$sploded = explode("'", $werd);
+			$werd = $sploded[0];
+		endif;
+		$werd = strtolower(trim(preg_replace('#[^\p{L}\p{N}]+#u', '', $werd)));
+		return $werd;
+	}
+	
+	
+	/*
+	// getStopWords:			Gets and sets the current array of stop words. By default, grabs the included text file and produces.
+	//							an array from that, to be saved to the options table.
+	*/
+	private function getStopWords() {
+		// Do we have stopwords in the db?
+		if($stopwords = get_option('hn_title_to_tags')) :
+			return $stopwords;
+		// If not, use the default list:
+		else :
+			$file 			= dirname(__FILE__).'/stopwords.txt';
+			$stopwords		= file_get_contents($file);
+			$verboten		= explode(',', $stopwords);
+			for($x = 0; $x < count($verboten); $x++) :
+				$verboten[$x]	= $this->lowerNoPunc($verboten[$x]);
+			endfor;
+			update_option('hn_title_to_tags', $verboten);
+			return $verboten;
+		endif;
+	}
+}
 ?>
